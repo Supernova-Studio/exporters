acf2153288f7d207e91d9450e3ca159892822c95
Marie Pauline Kristiansen <marie.pauline.kristiansen@nhn.no>
2024-04-30 16:15:41 +0200

    try to revert the format changes

--- exporters/css/src/content/token.ts
+++ exporters/css/src/content/token.ts
@@ -1,14 +1,10 @@
-import { NamingHelper, CSSHelper } from '@supernovaio/export-helpers';
-import { Token, TokenGroup } from '@supernovaio/sdk-exporters';
-import { exportConfiguration } from '..';
+import { NamingHelper, CSSHelper } from "@supernovaio/export-helpers"
+import { Token, TokenGroup } from "@supernovaio/sdk-exporters"
+import { exportConfiguration } from ".."
 
-export function convertedToken(
-  token: Token,
-  mappedTokens: Map<string, Token>,
-  tokenGroups: Array<TokenGroup>
-): string {
+export function convertedToken(token: Token, mappedTokens: Map<string, Token>, tokenGroups: Array<TokenGroup>): string {
   // First creating the name of the token, using helper function which turns any token name / path into a valid variable name
-  const name = tokenVariableName(token, tokenGroups);
+  const name = tokenVariableName(token, tokenGroups)
 
   // Then creating the value of the token, using another helper function
   const value = CSSHelper.tokenToCSS(token, mappedTokens, {
@@ -16,31 +12,23 @@ export function convertedToken(
     decimals: exportConfiguration.colorPrecision,
     colorFormat: exportConfiguration.colorFormat,
     tokenToVariableRef: (t) => {
-      return `var(--${tokenVariableName(t, tokenGroups)})`;
+      return `var(--${tokenVariableName(t, tokenGroups)})`
     },
-  });
-  const indentString = ' '.repeat(exportConfiguration.indent);
+  })
+  const indentString = " ".repeat(exportConfiguration.indent)
 
   if (exportConfiguration.showDescriptions && token.description) {
     // Generate token with comments
     // Newline before comment
-    return `\n${indentString}/* ${token.description.trim()} */\n${indentString}--${name}: ${value};`;
+    return `\n${indentString}/* ${token.description.trim()} */\n${indentString}--${name}: ${value};`
   } else {
     // Generate tokens without comments
-    return `${indentString}--${name}: ${value};`;
+    return `${indentString}--${name}: ${value};`
   }
 }
 
-function tokenVariableName(
-  token: Token,
-  tokenGroups: Array<TokenGroup>
-): string {
-  const prefix = exportConfiguration.tokenPrefixes[token.tokenType];
-  const parent = tokenGroups.find((group) => group.id === token.parentGroupId)!;
-  return NamingHelper.codeSafeVariableNameForToken(
-    token,
-    exportConfiguration.tokenNameStyle,
-    parent,
-    prefix
-  );
+function tokenVariableName(token: Token, tokenGroups: Array<TokenGroup>): string {
+  const prefix = exportConfiguration.tokenPrefixes[token.tokenType]
+  const parent = tokenGroups.find((group) => group.id === token.parentGroupId)!
+  return NamingHelper.codeSafeVariableNameForToken(token, exportConfiguration.tokenNameStyle, parent, prefix)
 }
