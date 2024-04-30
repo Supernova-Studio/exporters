70a78eb6e45abb7401fc404ed402e4adb79be187
Marie Pauline Kristiansen <marie.pauline.kristiansen@nhn.no>
2024-04-30 13:45:25 +0200

    feat: customize exporter to meet our stylelint rules

--- exporters/css/src/content/token.ts
+++ exporters/css/src/content/token.ts
@@ -1,10 +1,14 @@
-import { NamingHelper, CSSHelper } from "@supernovaio/export-helpers"
-import { Token, TokenGroup } from "@supernovaio/sdk-exporters"
-import { exportConfiguration } from ".."
+import { NamingHelper, CSSHelper } from '@supernovaio/export-helpers';
+import { Token, TokenGroup } from '@supernovaio/sdk-exporters';
+import { exportConfiguration } from '..';
 
-export function convertedToken(token: Token, mappedTokens: Map<string, Token>, tokenGroups: Array<TokenGroup>): string {
+export function convertedToken(
+  token: Token,
+  mappedTokens: Map<string, Token>,
+  tokenGroups: Array<TokenGroup>
+): string {
   // First creating the name of the token, using helper function which turns any token name / path into a valid variable name
-  const name = tokenVariableName(token, tokenGroups)
+  const name = tokenVariableName(token, tokenGroups);
 
   // Then creating the value of the token, using another helper function
   const value = CSSHelper.tokenToCSS(token, mappedTokens, {
@@ -12,22 +16,31 @@ export function convertedToken(token: Token, mappedTokens: Map<string, Token>, t
     decimals: exportConfiguration.colorPrecision,
     colorFormat: exportConfiguration.colorFormat,
     tokenToVariableRef: (t) => {
-      return `var(--${tokenVariableName(t, tokenGroups)})`
+      return `var(--${tokenVariableName(t, tokenGroups)})`;
     },
-  })
-  const indentString = " ".repeat(exportConfiguration.indent)
+  });
+  const indentString = ' '.repeat(exportConfiguration.indent);
 
   if (exportConfiguration.showDescriptions && token.description) {
     // Generate token with comments
-    return `${indentString}/* ${token.description.trim()} */\n${indentString}--${name}: ${value};`
+    // Newline before comment
+    return `\n${indentString}/* ${token.description.trim()} */\n${indentString}--${name}: ${value};`;
   } else {
     // Generate tokens without comments
-    return `${indentString}--${name}: ${value};`
+    return `${indentString}--${name}: ${value};`;
   }
 }
 
-function tokenVariableName(token: Token, tokenGroups: Array<TokenGroup>): string {
-  const prefix = exportConfiguration.tokenPrefixes[token.tokenType]
-  const parent = tokenGroups.find((group) => group.id === token.parentGroupId)!
-  return NamingHelper.codeSafeVariableNameForToken(token, exportConfiguration.tokenNameStyle, parent, prefix)
+function tokenVariableName(
+  token: Token,
+  tokenGroups: Array<TokenGroup>
+): string {
+  const prefix = exportConfiguration.tokenPrefixes[token.tokenType];
+  const parent = tokenGroups.find((group) => group.id === token.parentGroupId)!;
+  return NamingHelper.codeSafeVariableNameForToken(
+    token,
+    exportConfiguration.tokenNameStyle,
+    parent,
+    prefix
+  );
 }
--- exporters/css/src/files/style-file.ts
+++ exporters/css/src/files/style-file.ts
@@ -1,32 +1,55 @@
-import { FileHelper } from "@supernovaio/export-helpers"
-import { OutputTextFile, Token, TokenGroup, TokenType } from "@supernovaio/sdk-exporters"
-import { exportConfiguration } from ".."
-import { convertedToken } from "../content/token"
+import { FileHelper } from '@supernovaio/export-helpers';
+import {
+  OutputTextFile,
+  Token,
+  TokenGroup,
+  TokenType,
+} from '@supernovaio/sdk-exporters';
+import { exportConfiguration } from '..';
+import { convertedToken } from '../content/token';
 
-export function styleOutputFile(type: TokenType, tokens: Array<Token>, tokenGroups: Array<TokenGroup>): OutputTextFile | null {
+export function styleOutputFile(
+  type: TokenType,
+  tokens: Array<Token>,
+  tokenGroups: Array<TokenGroup>
+): OutputTextFile | null {
   // Filter tokens by top level type
-  const tokensOfType = tokens.filter((token) => token.tokenType === type)
+  const tokensOfType = tokens.filter((token) => token.tokenType === type);
+
+  // Only export supported token types
+  // TODO: Add all token types
+  const supportedTypes: Array<TokenType> = [TokenType.color];
+  if (!supportedTypes.includes(type)) {
+    return null;
+  }
 
   // Filter out files where there are no tokens, if enabled
   if (!exportConfiguration.generateEmptyFiles && tokensOfType.length === 0) {
-    return null
+    return null;
   }
 
   // Convert all tokens to CSS variables
-  const mappedTokens = new Map(tokens.map((token) => [token.id, token]))
-  const cssVariables = tokensOfType.map((token) => convertedToken(token, mappedTokens, tokenGroups)).join("\n")
+  const mappedTokens = new Map(tokens.map((token) => [token.id, token]));
+  const cssVariables = tokensOfType
+    .map((token) => convertedToken(token, mappedTokens, tokenGroups))
+    .join('\n');
+
+  // Custom stylelint rules
+  const contentRules = '/* stylelint-disable color-hex-length */\n';
 
   // Create file content
-  let content = `:root {\n${cssVariables}\n}`
+  let content = `:root {\n${cssVariables}\n}`;
   if (exportConfiguration.showGeneratedFileDisclaimer) {
     // Add disclaimer to every file if enabled
-    content = `/* ${exportConfiguration.disclaimer} */\n${content}`
+    content = `/* ${exportConfiguration.disclaimer} */\n${content}`;
   }
+  // Add stylelint rules on top of file
+  content = `${contentRules}\n${content}`;
 
   // Retrieve content as file which content will be directly written to the output
   return FileHelper.createTextFile({
     relativePath: exportConfiguration.baseStyleFilePath,
     fileName: exportConfiguration.styleFileNames[type],
     content: content,
-  })
+  });
 }
