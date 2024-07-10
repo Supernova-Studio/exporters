56ca46001bb83e1f25a3d7126d730177cdbe775b
Pablo Saez <pablo.saez@onum.com>
2024-07-10 14:24:16 +0200

    feat: theme dark/light

--- exporters/css/src/files/style-file.ts
+++ exporters/css/src/files/style-file.ts
@@ -1,26 +1,40 @@
-import { FileHelper } from "@supernovaio/export-helpers"
-import { OutputTextFile, Token, TokenGroup, TokenType } from "@supernovaio/sdk-exporters"
-import { exportConfiguration } from ".."
-import { convertedToken } from "../content/token"
+import { FileHelper } from "@supernovaio/export-helpers";
+import {
+  OutputTextFile,
+  Token,
+  TokenGroup,
+  TokenType,
+} from "@supernovaio/sdk-exporters";
+import { exportConfiguration } from "..";
+import { convertedToken } from "../content/token";
 
-export function styleOutputFile(type: TokenType, tokens: Array<Token>, tokenGroups: Array<TokenGroup>): OutputTextFile | null {
+export function styleOutputFile(
+  type: TokenType,
+  tokens: Array<Token>,
+  tokenGroups: Array<TokenGroup>
+): OutputTextFile | null {
   // Filter tokens by top level type
-  const tokensOfType = tokens.filter((token) => token.tokenType === type)
+  const tokensOfType = tokens.filter((token) => token.tokenType === type);
 
   // Filter out files where there are no tokens, if enabled
   if (!exportConfiguration.generateEmptyFiles && tokensOfType.length === 0) {
-    return null
+    return null;
   }
 
   // Convert all tokens to CSS variables
-  const mappedTokens = new Map(tokens.map((token) => [token.id, token]))
-  const cssVariables = tokensOfType.map((token) => convertedToken(token, mappedTokens, tokenGroups)).join("\n")
+  const mappedTokens = new Map(tokens.map((token) => [token.id, token]));
+  const cssVariables = tokensOfType
+    .map((token) => convertedToken(token, mappedTokens, tokenGroups))
+    .join("\n");
 
   // Create file content
-  let content = `:root {\n${cssVariables}\n}`
+  const theme = exportConfiguration.baseStyleFilePath.includes("dark")
+    ? "dark"
+    : "light";
+  let content = `[data-theme="${theme}"] {\n${cssVariables}\n}`;
   if (exportConfiguration.showGeneratedFileDisclaimer) {
     // Add disclaimer to every file if enabled
-    content = `/* ${exportConfiguration.disclaimer} */\n${content}`
+    content = `/* ${exportConfiguration.disclaimer} */\n${content}`;
   }
 
   // Retrieve content as file which content will be directly written to the output
@@ -28,5 +42,5 @@ export function styleOutputFile(type: TokenType, tokens: Array<Token>, tokenGrou
     relativePath: exportConfiguration.baseStyleFilePath,
     fileName: exportConfiguration.styleFileNames[type],
     content: content,
-  })
+  });
 }
